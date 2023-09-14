ef3d4900726cf35b5ba524f5355c37f4a495f316
Max Reiter <max.m.reiter@gmail.com>
2023-09-14 13:21:15 -0700

    document options

--- option.go
+++ option.go
@@ -1,49 +1,61 @@
 package gopster
 
+// Option configures a Chart.
 type Option func(*Chart)
 
+// Title sets a Chart's title. Defaults to an empty string.
 func Title(t string) Option {
 	return func(c *Chart) {
 		c.title = t
 	}
 }
 
+// Width sets a Chart's width. Defaults to 3.
 func Width(w int) Option {
 	return func(c *Chart) {
 		c.width = w
 	}
 }
 
+// Height sets a Chart's height. Defaults to 3.
 func Height(h int) Option {
 	return func(c *Chart) {
 		c.height = h
 	}
 }
 
+// ShowNumbers toggles number rendering on a Chart. Defaults to false.
+// ShowTitles must also be true for this to work.
 func ShowNumbers() Option {
 	return func(c *Chart) {
 		c.showNumbers = true
 	}
 }
 
+// ShowTitles toggles title rendering on a Chart. Defaults to false.
 func ShowTitles() Option {
 	return func(c *Chart) {
 		c.showTitles = true
 	}
 }
 
+// Gap sets a Chart's gap. Defaults to 20.
 func Gap(g float64) Option {
 	return func(c *Chart) {
 		c.gap = g
 	}
 }
 
+// BackgroundColor sets a Chart's background color. Must be a valid hex color string.
+// Defaults to black.
 func BackgroundColor(bc string) Option {
 	return func(c *Chart) {
 		c.backgroundColor = bc
 	}
 }
 
+// TextColor sets a Chart's text color. Must be a valid hex color string.
+// Defaults to white.
 func TextColor(tc string) Option {
 	return func(c *Chart) {
 		c.textColor = tc
